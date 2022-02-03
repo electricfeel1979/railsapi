0c7310351fe76a55edae82f1b8c2bdee8a11246f
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2022-02-04 01:05:53 +0800

    improve rspec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "tests a number to be a positive" do 
-    expect(1).to be_positive #be_positive => 1.posiive? == true
+  describe '#validations' do
+    let(:article) { build(:article) }
+
+    it "tests that factory is valid" do 
+      # article = build(:article)
+      expect(article).to be_valid # article.valid? == true
+    end
+  
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
   end
 end
