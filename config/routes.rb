98c09e11bb246ecdeac56044eb51fd0034aa1f7e
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2022-02-04 17:24:51 +0800

    more tests implementation

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/articles', to: 'articles#index'
+  # resources :articles, only: [:index]
+  resources :articles, only: %i[index show]
 end
