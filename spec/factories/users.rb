a8dfc0c560e6d0ce4be9366421ccfb97a7386e6e
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2022-02-08 17:22:25 +0800

    adder User object added tests

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:login) { |n| "kevinmm#{n}" }
+    name { "Kevin MM" }
+    url { "http://example.com" }
+    avatar_url { "http://example.com/avatar" }
+    provider { "github" }
+  end
+end
