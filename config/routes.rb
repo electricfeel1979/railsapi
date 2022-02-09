1033a895b9e9645eaff9d9e3c9fcb14f81710f5c
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2022-02-09 22:37:10 +0800

    added routing and controller of access_tokens

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # get '/articles', to: 'articles#index'
   # resources :articles, only: [:index]
+  post 'login', to: 'access_tokens#create'
   resources :articles, only: %i[index show]
 end
