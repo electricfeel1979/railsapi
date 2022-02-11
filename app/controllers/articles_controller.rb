cbf064eabb9a7a2bd94307d14ba780e61d52837c
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2022-02-12 02:49:38 +0800

    more test improvements

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
     include Paginable
 
+    skip_before_action :authorize!, only: [:index, :show]
+
     def index
         paginated = paginate(Article.recent)
         render_collection(paginated)
