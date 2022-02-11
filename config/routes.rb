e09c10ac3d1275d5585eb3ebd37b5d5a91990754
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2022-02-12 01:39:11 +0800

    more test implementation for access_tokens_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # get '/articles', to: 'articles#index'
   # resources :articles, only: [:index]
   post 'login', to: 'access_tokens#create'
+  delete 'logout', to: 'access_tokens#destroy'
   resources :articles, only: %i[index show]
 end
--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
-    describe "#create" do
+    describe "POST #create" do
         shared_examples_for 'unauthorized_requests' do
-            let(:error) do
+            let(:authentication_error) do
                 {
                     "status": "401",
                     "source": { "pointer": "/code" },
@@ -18,7 +18,7 @@ RSpec.describe AccessTokensController, type: :controller do
 
             it 'should return proper error body' do
                 subject
-                expect(json[:errors]).to include(error)
+                expect(json[:errors]).to include(authentication_error)
             end
         end
         context 'when no code provided' do
@@ -75,9 +75,38 @@ RSpec.describe AccessTokensController, type: :controller do
                 
                 # this is a fix for the tutorial
                 response_json = JSON.parse(response.body)
-                pp response_json
+                # pp response_json
                 expect(response_json['token']).to eq(user.access_token.token)
             end
+        end
+    end
+
+    describe 'DELETE #destroy' do
+        context 'when invalid request' do
+            let(:authorization_error) do 
+                {
+                    "status": "403",
+                    "source": { "pointer": "/headers/authorization" },
+                    "title":  "Not authorized",
+                    "detail": "You have no right to access this resource."
+                }
+            end
+
+            subject {delete :destroy}
+
+            it 'should return 403 status code' do
+                subject
+                expect(response).to have_http_status(:forbidden)
+            end
+
+            it 'should return proper error json' do
+                subject
+                expect(json[:errors]).to include(authorization_error)
+            end
+        end
+
+        context 'when valid request' do
+
         end
     end
 end
