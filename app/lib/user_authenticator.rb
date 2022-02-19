14f5640376cd50c13a923359185e5b6f4d819722
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2022-02-19 08:38:27 +0800

    64. Fixing our authentication service specs

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -1,52 +1,23 @@
 class UserAuthenticator
-    class AuthenticationError < StandardError; end
+    attr_reader :authenticator
 
-    attr_reader :user, :access_token
-
-    def initialize(code)
-        @code = code
+    def initialize(code: nil, login: nil, password: nil)
+        @authenticator = if code.present?
+            Oauth.new(code)
+        else
+            Standard.new(login, password)
+        end
     end
 
     def perform
-        raise AuthenticationError if code.blank?
-        raise AuthenticationError if token.try(:error).present?
-        prepare_user
-        @access_token = if user.access_token.present? 
-            user.access_token 
-        else
-            user.create_access_token
-        end
+        authenticator.perform
     end
 
-    private
-
-    def client
-        @client ||= Octokit::Client.new(
-            client_id: ENV['GITHUB_CLIENT_ID'],
-            client_secret: ENV['GITHUB_CLIENT_SECRET']
-        )
+    def user
+        authenticator.error
     end
 
-    def token
-        @token ||= client.exchange_code_for_token(code)
+    def access_token
+        authenticator.access_token
     end
-
-    def user_data
-        @user_data ||= Octokit::Client.new(:access_token => token).user.to_h.slice(:login, :avatar_url, :url, :name)
-    end
-
-    def prepare_user
-        @user = if User.exists?(login: user_data[:login])
-            User.find_by(login: user_data[:login])
-        else
-            User.create(user_data.merge(provider: 'github'))
-        end
-    end
-
-
-    attr_reader :code
 end
-
-
-# client = Octokit::Client.new(client_id: 'e37f7935ba4983e80da2', client_se
-#     cret: 'fe78d02450d4c6f0bf60e9841f86da21004d9839')
