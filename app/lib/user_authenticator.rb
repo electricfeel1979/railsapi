961f4fc5baa3bc4e6e96814c2542eebd6745e42f
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2022-02-21 05:04:42 +0800

    68. Standard Authentication flow: Access token preparation

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -1,23 +1,31 @@
 class UserAuthenticator
-    attr_reader :authenticator
-
+    attr_reader :authenticator, :access_token
+  
     def initialize(code: nil, login: nil, password: nil)
-        @authenticator = if code.present?
-            Oauth.new(code)
-        else
-            Standard.new(login, password)
-        end
+      @authenticator = if code.present?
+        Oauth.new(code)
+      else
+        Standard.new(login, password)
+      end
     end
-
+  
     def perform
-        authenticator.perform
+      authenticator.perform
+  
+      set_access_token
     end
-
+  
     def user
-        authenticator.error
+      authenticator.user
     end
-
-    def access_token
-        authenticator.access_token
+  
+    private
+  
+    def set_access_token
+      @access_token = if user.access_token.present?
+        user.access_token
+      else
+        user.create_access_token
+      end
     end
-end
+  end
--- spec/lib/user_authenticator/oauth_spec.rb
+++ spec/lib/user_authenticator/oauth_spec.rb
@@ -50,11 +50,6 @@ describe UserAuthenticator::Oauth do
                 expect{subject}.not_to change{User.count}
                 expect(authenticator.user).to eq(user)
             end
-
-            it "should create and set users access token" do
-                expect{subject}.to change{AccessToken.count}.by(1)
-                expect(authenticator.access_token).to be_present
-            end
         end
     end
 end
